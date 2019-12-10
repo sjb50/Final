19476665d40bae6cbab4a644ab79e2e1e847ee5d
Samuel Bedell <48493644+sjb50@users.noreply.github.com>
2019-12-09 18:37:57 -0600

    g

--- LibraryProject/src/GUI/Login.java
+++ LibraryProject/src/GUI/Login.java
@@ -33,7 +33,7 @@ public class Login extends JFrame implements Serializable {
 	/**
 	 * Launch the application.
 	 */
-	/**public static void main(String[] args) {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -44,7 +44,7 @@ public class Login extends JFrame implements Serializable {
 				}
 			}
 		});
-	}**/
+	}
 
 	/**
 	 * Create the frame.
--- LibraryProject/src/Person/TestDriver.java
+++ LibraryProject/src/Person/TestDriver.java
@@ -5,7 +5,7 @@ import helpers.Sorting;
 
 public class TestDriver {
 
-	public static void main(String[] args) {
+	/**public static void main(String[] args) {
 		Book book1 = new Book("abe", "erer", "hello");
 		Book book2 = new Book("bob", "deer", "goodbye");
 		Book book3 = new Book("cat", "cow", "gbye");
@@ -18,5 +18,5 @@ public class TestDriver {
 		bookCollection.add(book4);
 		Sorting.bookMergeSortByTitle(bookCollection.getBooks(),0,bookCollection.getManyBooks());
 		System.out.println(bookCollection.SearchByTitle(book2));
-	}
+	}**/
 }
