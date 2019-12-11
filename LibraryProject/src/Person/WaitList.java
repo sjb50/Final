a44a7b4edf7fbc6e3476198f8346fb0da4952d67
jorelngoula <40173048+jorelngoula@users.noreply.github.com>
2019-12-11 16:14:28 -0600

    Update WaitList.java

--- LibraryProject/src/Person/WaitList.java
+++ LibraryProject/src/Person/WaitList.java
@@ -17,12 +17,26 @@ import java.io.Serializable;
 public class WaitList implements Serializable{
 	PersonNode next;
 	PersonNode last;
-
+	/**
+	 *@Specifications: no argument constructor that initializes the instance variables
+	 *@Param: none
+	 *@Precondition: the instance variables must exist
+	 *@Postcondition: sets the instance variables to null
+	 *@Exceptions: none
+	 *@Throws: none
+	 */
 	public WaitList() {
 		next = null;
 		last = null;
 	}
-
+	/**
+	 *@Specifications: Method that adds a member to the queue
+	 *@Param: takes a Person argument
+	 *@Precondition: the person must exist
+	 *@Postcondition: sets the person to the first place if the waitlist is empty
+	 *@Exceptions: none
+	 *@Throws: none
+	 */
 	public void enque(Person person) {
 		PersonNode newPerson = new PersonNode(person);
 		if (next == null) {
@@ -33,13 +47,27 @@ public class WaitList implements Serializable{
 			last = newPerson;
 		}
 	}
-
+	/**
+	 *@Specifications: method that removes a person from the waitlist
+	 *@Param: none
+	 *@Precondition: the person must be present in the waitlist
+	 *@Postcondition: retruns the person's information
+	 *@Exceptions: none
+	 *@Throws: none
+	 */
 	public Person remove() {
 		Person person = next.getPerson();
 		next = next.getLink();
 		return person;
 	}
-
+	/**
+	 *@Specifications: method that reads the members in the waitlist
+	 *@Param: none
+	 *@Precondition: the list must exist
+	 *@Postcondition: returns the information of the list
+	 *@Exceptions: none
+	 *@Throws: none
+	 */
 	public String readQue() {
 		String info = "";
 		PersonNode cursor = next;
@@ -49,7 +77,14 @@ public class WaitList implements Serializable{
 		}
 		return info;
 	}
-
+	/**
+	 *@Specifications: method that makes sure the waitlist is not empty before it searches through
+	 *@Param: none
+	 *@Precondition: the list must exist
+	 *@Postcondition: returns true if the list is not empty
+	 *@Exceptions: none
+	 *@Throws: none
+	 */
 	public boolean hasNext() {
 		if (next != null) {
 			return true;
@@ -57,5 +92,4 @@ public class WaitList implements Serializable{
 			return false;
 		}
 	}
-
 }
